f08a90dff7dbdb7cece7b8b4e82bd1a072587717
Julio Silveira Melo <juliocsmelo@gmail.com>
2019-11-21 23:09:24 +0100

    chore: added argparser

--- k8s_secret_gen/k8s_secret_gen.py
+++ k8s_secret_gen/k8s_secret_gen.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
 import yaml
 import base64
-from .lib import create_secrets
+import argparse
+import pathlib
+
+from lib import create_secrets, create_container_env_vars, create_secret_yaml
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Simple cli command to convert Kubernetes container environment variables to secrets')
+    parser.add_argument('-f', '--file', help='Deployment YAML',
+                        required=True, type=pathlib.Path)
+    args = parser.parse_args()
+    yaml_data = create_secrets(args.file)
+    secret_yaml = create_secret_yaml(
+        yaml_data['secret']['name'], yaml_data['secret']['b64_secrets'])
+    deployment_env_vars = create_container_env_vars(yaml_data['env_vars'])
+    print(secret_yaml)
+    print("---")
+    print(deployment_env_vars)
+    print("---")
+
 
 if __name__ == '__main__':
-    create_secrets('test.yaml')
+    main()
